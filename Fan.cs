8a3574016707db611684a61a5b8eed9fda0de4e8
trung6755 <trungggrr@gmail.com>
2024-03-05 16:10:20 +0700

    Initial commit

---
+++ Fan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Fan
+{
+    public class Fan
+    {
+        private string brand {  get; set; }
+        private string color {  get; set; }
+        public bool isOn = false;
+        private int speed = 0;
+
+        public Fan(string brand, string color)
+        {
+            isOn = false;
+            this.brand = brand;
+            this.color = color;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("This is a " + this.brand + " fan and its color is " + this.color);
+            if (isOn)
+            {
+                Console.WriteLine("The fan speed is: " + speed);
+            }
+        }
+
+        public void turnON()
+        {
+            isOn = true;
+            Console.Write("Enter speed: ");
+            speed = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fan fan = new Fan("Mitsubishi", "Black");
+            fan.turnON();
+            fan.Info();
+            
+        }
+    }
+}
